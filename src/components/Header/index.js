2f4cf4beeff2d5b8e24f5b2594993774ef525fef
Anthony Bergen <anthonydbergen@gmail.com>
2020-09-22 19:45:51 -0400

    updated header directions

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,8 +5,10 @@ function Header() {
   return (
     <div className="header">
       <h1>Employee Directory</h1>
-      <h5>Click on a category to sort by ascending or descending</h5>
-      <h5>Or search for a name to filter user by name</h5>
+      <h5>
+        Click on a category to sort columns by ascending or descending order.
+      </h5>
+      <h5>Or search by name to filter users by name.</h5>
     </div>
   );
 }
