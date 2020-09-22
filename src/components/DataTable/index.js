670e74fc785d203082c8864ed92d492b3947cdbb
Anthony Bergen <anthonydbergen@gmail.com>
2020-09-22 11:32:00 -0400

    completed DataTable component

--- src/components/DataTable/index.js
+++ src/components/DataTable/index.js
@@ -1,13 +1,41 @@
 import React from "react";
-import DataArea from "/DataArea";
+import DataBody from "../DataBody";
+import DataAreaContext from "../../utils/DataAreaContext";
 import "./style.css";
 
-function Main() {
+const DataTable = () => {
+  const context = useContext(DataAreaContext);
+
   return (
-    <div>
-      <DataArea />
+    <div className="datatable mt-5">
+      <table
+        id="table"
+        className="table table-striped table-hover table-condensed"
+      >
+        <thead>
+          <tr>
+            {oncontextmenu.developerState.headings.map(({ name, width }) => {
+              return (
+                <th
+                  className="col"
+                  key={name}
+                  style={{ width }}
+                  onClick={() => {
+                    context.handleSort(name);
+                  }}
+                >
+                  {name}
+                  <span className="pointer"></span>
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
+
+        <DataBody />
+      </table>
     </div>
   );
-}
+};
 
-export default Main;
+export default DataTable;
