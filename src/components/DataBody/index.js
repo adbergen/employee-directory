11cb5e7e84ef10f436f0852582e7794163dba690
Anthony Bergen <anthonydbergen@gmail.com>
2020-09-22 10:57:00 -0400

    added skeleton data components to begin pulling data from api

---
+++ src/components/DataBody/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DataArea from "/DataArea";
+import "./style.css";
+
+function Main() {
+  return (
+    <div>
+      <DataArea />
+    </div>
+  );
+}
+
+export default Main;
