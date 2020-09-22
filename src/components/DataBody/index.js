ada14f8d874ec05299e7ac986fdd7a0c5c5a872b
Anthony Bergen <anthonydbergen@gmail.com>
2020-09-22 11:21:41 -0400

    completed DataBody component

--- src/components/DataBody/index.js
+++ src/components/DataBody/index.js
@@ -6,7 +6,7 @@ const DataBody = () => {
   const context = useContext(DataAreaContext);
 
   function formatDate(date) {
-    const dataArray = date.split("-");
+    const dateArray = date.split("-");
     const year = dateArray[0];
     const month = dateArray[1];
     const dayArray = dateArray[2].split("T");
@@ -15,7 +15,44 @@ const DataBody = () => {
     return formattedDate;
   }
 
-  return <tbody></tbody>;
+  return (
+    <tbody>
+      {context.developerState.filteredUsers[0] !== undefined &&
+      context.developerState.filteredUsers[0].name !== undefined ? (
+        context.developerState.filteredUsers.map(
+          ({ login, name, picture, phone, email, dob }) => {
+            return (
+              <tr key={login.uuid}>
+                <td data-th="Image" className="align-middle">
+                  <img
+                    src={picture.medium}
+                    alt={"profile image for " + name.first + " " + name.last}
+                    className="img-responsive"
+                  />
+                </td>
+                <td data-th="Name" className="name-cell align-middle">
+                  {name.first} {name.last}
+                </td>
+                <td data-th="Phone" className="align-middle">
+                  {phone}
+                </td>
+                <td data-th="Email" className="align-middle">
+                  <a href={"mailto:" + email} target="__blank">
+                    {email}
+                  </a>
+                </td>
+                <td data-th="DOB" className="align-middle">
+                  {formatDate(dob.date)}
+                </td>
+              </tr>
+            );
+          }
+        )
+      ) : (
+        <></>
+      )}
+    </tbody>
+  );
 };
 
 export default DataBody;
